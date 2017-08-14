9bdc4e447f1afbc2d7cd3ba37fdd2fe76d7330a2
Robert van der Mark <robert.vandermark@gmail.com>
2017-08-14 10:38:08 -0400

    findUserViewModel test

--- db.js
+++ db.js
@@ -1,18 +1,34 @@
 const Sequelize = require('sequelize');
 // const db = new Sequelize(process.env.DATABASE_URL);
 
-const db = new Sequelize('postgres://localhost/acme_mentors')
+const db = new Sequelize('postgres://localhost/acme_mentors', {logging: false})
 
 const User = db.define('user', {
 	name: Sequelize.STRING
 });
 
+User.findUsersViewModel = function() {
+  return User.findAll({
+  	include: [{
+  		model: User, 
+			as: "mentor",
+			attributes: ['name', 'id']
+		}, {model: Award, as: 'award', attributes: ['title', 'userId']}]
+  })
+  .then( (users) => {
+  	return users;
+  } )
+};
+
 const Award = db.define('award', {
 	title: Sequelize.STRING
 });
 
-Award.belongsTo(User);
-User.hasOne(User, {as: 'mentor'});
+
+// Award.belongsTo(User, {as: 'user', foreignKey: 'userId' });
+// User.hasOne(User, { as: 'mentee', foreignKey: 'menteeId'});
+User.belongsTo(User,{ as: 'mentor', foreignKey: 'mentorId'});
+User.hasMany(Award, {as: 'award', foreignKey: 'userId' });
 
 const sync = () => db.sync({force: true});
 
@@ -28,7 +44,8 @@ const seed = () => {
 			Award.create({title: 'Wicked-good Wizard', userId: gandolf.id}),
 			Award.create({title: 'Bearer of the Ring', userId: frodo.id}),
 			Award.create({title: 'Disappearing Hobbit', userId: frodo.id}),
-			User.update({mentorId: frodo.id}, {where: {id: sam.id}}),
+			User.update({mentorId: gandolf.id}, {where: {id: sam.id}}),
+			User.update({mentorId: gandolf.id}, {where: {id: frodo.id}}),
 		])
 	})
 	.catch(err => console.log(err))
@@ -37,6 +54,5 @@ const seed = () => {
 module.exports = {
 	sync,
 	seed,
-	User,
-	Award
+	models: { User, Award},
 };
--- test/models.js
+++ test/models.js
@@ -1,19 +1,53 @@
 const mocha = require('mocha');
 const expect = require('chai').expect;
 const db = require('../db');
-
-
-
+const User = db.models.User;
+const Award = db.models.Award;
 
 describe('Acme Users Mentors', function(){
 	describe('seeding:', function(){
+		before(()=> {
+    	return db.sync()
+      .then(()=> {
+      	return db.seed();
+      } );
+
+  	});
 		it('should have 3 users', function(){
-			return db.User.findAll()
+			return User.findAll()
 			.then(users => {
 				expect(users.length).to.equal(3);
-			}) 
+			}); 
+		});
+		it('should have 4 awards', function(){
+			return Award.findAll()
+			.then(awards => {
+				expect(awards.length).to.equal(4);
+			}); 
+		});
+		it('Sam should have Gandolf as a mentor', function(){
+			return User.findOne({
+				where: {name: "Samwise Gamgee"},
+				include: [{model: User, 
+					as: "mentor",
+					attributes: ['name', 'id']
+				}]
+			})
+			.then((user) => {
+				expect(user.mentor.name).to.equal('Gandolf the White');
+			});  
+		});
+		it('findUsersViewModel() returns viewModel', function(){
+			return User.findUsersViewModel() 
+			.then(viewModel => {
+				console.log('viewModel[1].award.title = ', viewModel[1].award[0].title)
+				expect(viewModel[1].mentor.name).to.equal('Gandolf the White');
+				expect(viewModel[1].award[0].title).to.equal('Fellowship of the Ring');
+				expect(viewModel[1].mentor.id).to.equal(3);
+				expect(viewModel.length).to.equal(3);
+				expect(viewModel[0].id).to.equal(3);
+			})
+			.catch((err) => console.error(err))
 		})
-	})
-
+	});
 })
-
